f2eaa27fefe89dabb5c72a3e72fdba8db9cf7bee
NathteeP <whatisitz.plus@gmail.com>
2024-06-19 12:26:17 +0700

    add feature:not-found

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const notFound = require('./middlewares/notfound')
 const app = express()
 
 
+app.use(notFound)
 
 const port = 8000
 app.listen(port, () => console.log('Server is running on',port))
