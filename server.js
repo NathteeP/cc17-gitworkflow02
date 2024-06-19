05fdce697adadd5c8c5703daf4101915737054b1
PUMIN-L <puminbg@gmail.com>
2024-06-19 12:26:06 +0700

    create-error-middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const errorMiddleware = require('./middlewares/error-middleware')
 const app = express()
 
 
+app.use(errorMiddleware)
 
 const port = 8000
-app.listen(port, () => console.log('Server is running on',port))
+app.listen(port, () => console.log('Server is running on', port))
